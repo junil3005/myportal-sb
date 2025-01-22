7c5d801abe007714c15f013c7660127634ab04fd
junil3005 <yjl3005@naver.com>
2025-01-22 12:28:01 +0900

    애플리케이션 설정

---
+++ src/main/java/himedia/mypotal/MypotalSbApplication.java
@@ -0,0 +1,13 @@
+package himedia.mypotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypotalSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypotalSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/himedia/mypotal/MypotalSbApplicationTests.java
@@ -0,0 +1,13 @@
+package himedia.mypotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypotalSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
